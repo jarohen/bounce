df077de4438ca043ca87f92a41d7e202251aae70
James Henderson <james@jarohen.me.uk>
2015-11-20 08:49:02 +0000

    Initial set of skeleton functions

--- src/bounce/core.clj
+++ src/bounce/core.clj
@@ -1,1 +1,54 @@
 (ns bounce.core)
+
+(def ^:private ^:dynamic *system*
+  {})
+
+(defn ->component
+  ([v]
+   (->component v (fn [])))
+  ([v close!]
+   ))
+
+(defn using [component dependencies]
+  )
+
+(defn with-component [component f]
+  )
+
+(defn with-system [system f]
+  )
+
+(defn make-system [dep-map]
+  )
+
+(defn ask [k & ks]
+  )
+
+(defn system []
+  )
+
+(defn set-system-fn! [f]
+  )
+
+(defn start! []
+  )
+
+(defn stop! []
+  )
+
+(defn reload! []
+  )
+
+;; example code
+
+(comment
+  (defn foo-make-my-system []
+    (b/make-system {:config (fn []
+                              (b/->component ...))
+
+                    :server (-> (fn []
+                                  (b/->component ...
+                                                 (fn []
+                                                   ...)))
+
+                                (b/using #{:config}))})))
