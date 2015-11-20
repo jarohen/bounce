267396bc41403a162075f669c2e5f4c74e104e1a
James Henderson <james@jarohen.me.uk>
2015-11-20 08:24:57 +0000

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject jarohen/bounce "0.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://github.com/jarohen/bounce"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]])
---
+++ src/bounce/core.clj
@@ -0,0 +1,1 @@
+(ns bounce.core)
