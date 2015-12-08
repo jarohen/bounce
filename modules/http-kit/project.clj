a1af1a41eec7f3891e2df8cfafd4fdf97ed39cc4
James Henderson <james@jarohen.me.uk>
2015-12-08 09:35:41 +0000

    [http-kit] Fixing core snapshot

--- modules/http-kit/project.clj
+++ modules/http-kit/project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [jarohen/bounce "0.0.1-SNAPSHOT"]
+                 [jarohen/bounce "0.0.1-20151207.142349-5"]
                  [http-kit "2.1.19"]
                  [org.clojure/tools.logging "0.3.1"]])
