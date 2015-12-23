1fca56325f2d1cbff520e86472612e9ffbb821d1
James Henderson <james@jarohen.me.uk>
2015-12-23 00:13:19 +0000

    WIP: a first pass at a multi-page app

---
+++ examples/multi-page/ui-src/multi_page/ui/app.cljs
@@ -0,0 +1,86 @@
+(ns multi-page.ui.app
+  (:require [clojure.string :as s]
+            [bounce.core :as bc]
+            [bounce.mux :as mux]
+            [bounce.mux.bidi :as mux.bidi]
+            [reagent.core :as r]))
+
+(enable-console-print!)
+
+(defn page-component []
+  (or (when-let [root-component @(r/cursor (bc/ask :!app) [::root-component])]
+        [root-component])
+      [:div]))
+
+(defn render-page! []
+  (r/render-component [page-component] js/document.body))
+
+(defn home-page [{:keys [!count]}]
+  [:div
+   "Home Page"
+   [:p
+    [:a (mux/link (bc/ask :router) {:handler ::page-two
+                                    :route-params {:id 0}})
+     "Page 2"]]
+
+   [:p "Count: " (pr-str @!count)]
+   [:p [:button {:on-click #(swap! !count inc)}
+        "Inc"]]])
+
+(defn page-two []
+  [:div
+   "Page 2"
+   [:p
+    [:a (mux/link (bc/ask :router) {:handler ::home-page})
+     "Home page"]]
+   [:p
+    [:a (mux/link (bc/ask :router) {:handler ::page-two
+                                    :route-params {:id 1}})
+     "Page 2 (id = 1)"]]])
+
+(defn make-system-map []
+  {:!app (fn []
+           (bc/->component (r/atom {})))
+
+   :router (-> (fn []
+                 (mux/make-router {:token-mapper (mux.bidi/token-mapper ["" {"/" ::home-page
+                                                                             "/page-two" {["/" :id] ::page-two}}])
+                                   :listener #(reset! (r/cursor (bc/ask :!app) [::root-component]) %)
+
+                                   :default-location {:handler ::home-page}
+
+                                   :pages {::home-page (fn [{:keys [old-location new-location same-handler?]}]
+                                                         (let [!count (r/cursor (bc/ask :!app) [::home-count])]
+                                                           (when-not same-handler?
+                                                             (reset! !count 0))
+
+                                                           (mux/->page (fn []
+                                                                         [home-page {:!count !count}])
+
+                                                                       (fn [{:keys [old-location new-location same-handler?]}]
+                                                                         (when-not same-handler?
+                                                                           (swap! (bc/ask :!app) dissoc ::home-count))))))
+
+                                           ::page-two (fn [{:keys [old-location new-location same-handler?]}]
+                                                        (when-not same-handler?
+                                                          (println "mount page-2"))
+
+                                                        (mux/->page (fn []
+                                                                      [page-two])
+
+                                                                    (fn [{:keys [old-location new-location same-handler?]}]
+                                                                      (when-not same-handler?
+                                                                        (println "unmount page 2!")))))}}))
+               (bc/using #{:!app}))
+
+   :renderer (-> (fn []
+                   (bc/->component (render-page!)
+                                   (fn []
+                                     (r/unmount-component-at-node js/document.body))))
+
+                 (bc/using #{:!app}))})
+
+(set! (.-onload js/window)
+      (fn []
+        (bc/set-system-map-fn! #(make-system-map))
+        (bc/start!)))
