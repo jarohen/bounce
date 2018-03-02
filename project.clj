3458e2dd9ac8cdd50f94e00afa9e8b5ebd22b152
James Henderson <james@jarohen.me.uk>
2018-03-02 15:49:42 +0000

    setting to 0.0.1-SNAPSHOT s.t. we can deploy more easily

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jarohen/bounce "0.0.1-alpha5"
+(defproject jarohen/bounce "0.0.1-SNAPSHOT"
   :description "Bounce is a protocol-less, function composition-based alternative to Component"
   :url "https://github.com/jarohen/bounce"
   :license {:name "Eclipse Public License"
