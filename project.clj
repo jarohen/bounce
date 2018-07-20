ffaeee7e933adda61d1699a0a00d49a20158ddc7
James Henderson <james@jarohen.me.uk>
2018-07-20 08:37:01 +0100

    To deps.edn

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject jarohen/bounce "0.0.1-SNAPSHOT"
-  :description "Bounce is a protocol-less, function composition-based alternative to Component"
-  :url "https://github.com/jarohen/bounce"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.namespace "0.2.11"]
-                 [com.stuartsierra/dependency "0.2.0"]])
